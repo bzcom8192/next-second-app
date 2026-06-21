1b2e8eccea6a85049b75d380ee03feecc218d733
bzcom8192 <bzcom8192@gmail.com>
2026-06-21 15:43:10 +0700

    update product links

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -6,11 +6,11 @@ export default function Page() {
     <div>
       <NavBarMain />
       Product
-      <div className="flex flex-col gap-4 pt-4">
+      <div className="flex flex-col gap-2 pt-4 w-max">
           {["tablets", "notebooks", "pcs", "notebooks"].map((item) => (
-            <div key={item}>
+            <span key={item} className="hover:bg-gray-200 px-2 py-1 rounded-md">
               <Link href={`/product/${item}`}>{item}</Link>
-            </div>
+            </span>
           ))}
       </div>
     </div>
